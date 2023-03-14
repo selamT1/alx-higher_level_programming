ce10b34fbc899ddd24c1e518bb2ccfd6dca61f21
selamT1 <solla.tsg14@gmail.com>
2023-03-14 14:02:01 +0000

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,12 +3,11 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    ops = {'+': add, '-': sub, '*': mul, '/': div}
-
     if len(sys.argv) - 1 != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b')
         sys.exit(1)
 
+    ops = {'+': add, '-': sub, '*': mul, '/': div}
     if (sys.argv[2] not in list(ops.keys())):
         print('Unknown operator. Available operators: +, -, * and /')
         sys.exit(1)
