fb5ca8d2fdde8321ccd265d1888358a343a08949
selamT1 <solla.tsg14@gmail.com>
2023-03-17 18:45:50 +0000

    update

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
+
+
+def only_diff_elements(set_1, set_2):
     new_list = []
     [new_list.append(i)for i in set_1 if i not in set_2]
     [new_list.append(i)for i in set_2 if i not in set_1]
